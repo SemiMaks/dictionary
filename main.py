cb1e1d81c40c4cb90e42c9f1eceeefa98fd60ab7
Maksim <semimaks@mail.ru>
2022-11-12 21:58:31 +0300

    Добавлены функции

--- main.py
+++ main.py
@@ -1,33 +1,40 @@
 from words import words
 import random
 
-br = ('-' * 40)
+answer = 0
 
-print(br)
-print("Добро пожаловать в наш тренажёр!")
-print("Сколько слов готовы перевести?:")
-print(br)
-answer = input()
-answer = int(answer)
-print("Ваш выбор: ", answer, "слов(а). Удачи!")
 
-count = 0
+def greeting(answer):
+    br = ('-' * 40)
+    print(br)
+    print("Добро пожаловать в наш тренажёр!")
+    print("Сколько слов готовы перевести?:")
+    print(br)
+    answer = input()
+    answer = int(answer)
+    print("Ваш выбор: ", answer, "слов(а). Удачи!")
+    train(answer)
 
-for i in range(answer):
 
-    word, key = random.choice(list(words.items()))
+def train(answer):
+    count = 0
+    for i in range(answer):
 
-    print("\nВведите перевод слова -", word)
-    ans_word = input()
-    ans_word = str.lower(ans_word)
+        word, key = random.choice(list(words.items()))
 
-    if ans_word == key:
-        count += 1
-        print("Вы правы, ответом на слово -", word, "будет ", key)
-        print("Правильно переведено -", count, " слов(а)")
-    else:
-        print("К сожалению вы ошиблись, и переводом слова -", word, "станет -", key)
-        print("Вы ввели правилный перевод -", count, " слов")
+        print("\nВведите перевод слова -", word)
+        ans_word = input()
+        ans_word = str.lower(ans_word)
 
-print("\nПоздравляю, вы перевели ", count, " слов из ", answer)
-print("\nРабота программы завершена.\nЖелаю удачи!")
+        if ans_word == key:
+            count += 1
+            print("Вы правы, ответом на слово -", word, "будет ", key)
+            print("Правильно переведено -", count, " слов(а)")
+        else:
+            print("К сожалению вы ошиблись, и переводом слова -", word, "станет -", key)
+            print("Вы ввели правилный перевод -", count, " слов")
+
+    print("\nПоздравляю, вы перевели ", count, " слов из ", answer)
+    print("\nРабота программы завершена.\nЖелаю удачи!")
+
+greeting(answer)
