ecdc597117014349fa286d696b41a8fe1a51881d
Maksim <semimaks@mail.ru>
2022-11-17 04:12:15 +0300

    Правка доп функции

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from words import words
 import random
 
+print("Добро пожаловать в наш тренажёр!")
+
 
 def change():
     try:
-        print("Добро пожаловать в наш тренажёр!")
-        print("Выберете режим перевода.\nПерводим с английского языка на русский (1) или с русского на английский (2):")
+        print(
+            "Выберите режим перевода.\nПереводим с английского языка на русский (1) или с русского на английский (2):")
         ch = int(input())
         if (ch <= 0) or (ch > 2):
             print("Выберите пожалуйста '1' или '2'!")
@@ -29,47 +31,20 @@ def greeting(ch):
             greeting(ch)
         else:
             print("Ваш выбор: ", answer, "слов(а). Удачи!")
-            if ch == 1:
-                eng_to_rus(answer)
-            elif ch == 2:
-                rus_to_eng(answer)
+            train(answer, ch)
+
     except Exception as eror:
         print("Работа программы вызвала ошибку:", eror)
 
 
-def eng_to_rus(answer):
+def train(answer, ch):
     try:
         count = 0
         for i in range(answer):
-
-            word, key = random.choice(list(words.items()))
-
-            print("\nВведите перевод слова -", word)
-            ans_word = input()
-            ans_word = str.lower(ans_word)
-
-            if ans_word == key:
-                count += 1
-                print("Вы правы, ответом на слово -", word, "будет ", key)
-                print("Правильно переведено -", count, " слов(а)")
-            else:
-                print("К сожалению вы ошиблись, и переводом слова -", word, "станет -", key)
-                print("Вы ввели правилный перевод -", count, " слов")
-
-        print("\nПоздравляю, вы перевели ", count, " слов из ", answer)
-
-    except Exception as eror:
-        print("Работа программы прервана ошибкой ", eror)
-    finally:
-        print("Работа программы успешно завершена, удачи!")
-
-
-def rus_to_eng(answer):
-    try:
-        count = 0
-        for i in range(answer):
-
-            key, word = random.choice(list(words.items()))
+            if ch == 1:
+                word, key = random.choice(list(words.items()))
+            elif ch == 2:
+                key, word = random.choice(list(words.items()))
 
             print("\nВведите перевод слова -", word)
             ans_word = input()
